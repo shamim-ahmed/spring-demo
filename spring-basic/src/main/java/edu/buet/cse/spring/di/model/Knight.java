1a12f106af979d14abd41c07d9ac9c9142d8a138
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-03-25 18:17:29 +0600

    deleted some unnecessary files

--- spring-basic/src/main/java/edu/buet/cse/spring/di/model/Knight.java
+++
@@ -1,5 +0,0 @@
-package edu.buet.cse.spring.di.model;
-
-public interface Knight {
-  void embarkOnQuest();
-}
--- spring-basic/src/main/java/edu/buet/cse/spring/di/model/Quest.java
+++
@@ -1,5 +0,0 @@
-package edu.buet.cse.spring.di.model;
-
-public interface Quest {
-  void embark();
-}
