02bdbf11111a980807b3da59e501f7f57be44366
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-03-26 12:43:26 +0600

    added new classes to demonstrate the use of init and destroy methods via XML configuration

---
+++ spring-idol/src/main/java/edu/buet/cse/springidol/impl/Auditorium.java
@@ -0,0 +1,15 @@
+package edu.buet.cse.springidol.impl;
+
+public class Auditorium {
+  public void turnOnLights() {
+	System.out.println("Lights are being turned on...");
+  }
+  
+  public void use() {
+	System.out.println("The auditorium is being used...");
+  }
+  
+  public void turnOffLights() {
+	System.out.println("Lights are being turned off...");
+  }
+}
