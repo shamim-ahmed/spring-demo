ddc3fcdb3bd6ebf5ac642f4a5b64c9c7ab4d56f0
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-03-29 00:25:27 +0600

    added an example of autowiring by name

--- spring-ch03/src/main/java/edu/buet/cse/spring/ch03/App.java
+++ spring-ch03/src/main/java/edu/buet/cse/spring/ch03/App.java
@@ -1,7 +1,21 @@
 package edu.buet.cse.spring.ch03;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.context.ApplicationContext;
+
+import edu.buet.cse.spring.ch03.ex.PerformanceException;
+import edu.buet.cse.spring.ch03.model.Performer;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch03/spring-beans.xml");
+    Performer kenny = (Performer) appContext.getBean("kenny");
+    
+    try {
+      kenny.perform();
+    } catch (PerformanceException ex) {
+      ex.printStackTrace(System.err);
+    }
   }
 }
