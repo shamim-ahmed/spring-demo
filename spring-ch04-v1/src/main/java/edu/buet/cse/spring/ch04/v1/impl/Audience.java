2ef45118e37afc76878716f0d37e14d133912b70
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-02 12:30:13 +0600

    added a more elaborate example of AOP configuration, using both aop:before and aop:after XML directives

---
+++ spring-ch04-v1/src/main/java/edu/buet/cse/spring/ch04/v1/impl/Audience.java
@@ -0,0 +1,19 @@
+package edu.buet.cse.spring.ch04.v1.impl;
+
+public class Audience {
+  public void takeSeats() {
+    System.out.println("The audience is taking their seats");
+  }
+  
+  public void turnOffCellPhones() {
+    System.out.println("The audience is turning off their cell phones");
+  }
+  
+  public void applaud() {
+    System.out.println("Audience : CLAP CLAP CLAP");
+  }
+  
+  public void demandRefund() {
+    System.out.println("Audience : BOO ! We want our money back !");
+  }
+}
