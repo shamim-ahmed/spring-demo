a56dfe97d01329b18e2c81d44fba15af1ae3f7b8
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-02 18:35:53 +0600

    simplified the access of the "extra" method in the augmented object

--- spring-ch04-v2/src/main/java/edu/buet/cse/spring/ch04/v2/App3.java
+++ spring-ch04-v2/src/main/java/edu/buet/cse/spring/ch04/v2/App3.java
@@ -3,15 +3,12 @@ package edu.buet.cse.spring.ch04.v2;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import edu.buet.cse.spring.ch04.v2.model.Contestant;
+
 public class App3 {
   public static void main(String... args) {
     ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch04/v2/spring-beans.xml");
-    Object yanni = appContext.getBean("yanni");
-    
-    try {
-      yanni.getClass().getMethod("receiveAward").invoke(yanni);
-    } catch (Exception ex) {
-      ex.printStackTrace(System.err);
-    }
+    Contestant yanni = appContext.getBean("yanni", Contestant.class);
+    yanni.receiveAward();
   }
 }
