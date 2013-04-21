2b75c7990a99cc2abd8c9ec3b7912ae3cf28b4a9
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-21 23:53:41 +0600

    now we retrieve the DAO implementation from the application context using the interface name

--- spring-ch05-v1/src/main/java/edu/buet/cse/spring/ch05/v1/App.java
+++ spring-ch05-v1/src/main/java/edu/buet/cse/spring/ch05/v1/App.java
@@ -3,13 +3,13 @@ package edu.buet.cse.spring.ch05.v1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import edu.buet.cse.spring.ch05.v1.dao.ChirperDaoImpl;
+import edu.buet.cse.spring.ch05.v1.dao.ChirperDao;
 import edu.buet.cse.spring.ch05.v1.model.User;
 
 public class App {
   public static void main(String[] args) {
     ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch05/v1/spring-beans.xml");
-    ChirperDaoImpl chirperDao = appContext.getBean("chirperDao", ChirperDaoImpl.class);
+    ChirperDao chirperDao = appContext.getBean("chirperDao", ChirperDao.class);
     User user = chirperDao.getUser(1L);
     System.out.println(user);
   }
--- spring-ch05-v1/src/main/java/edu/buet/cse/spring/ch05/v1/App3.java
+++ spring-ch05-v1/src/main/java/edu/buet/cse/spring/ch05/v1/App3.java
@@ -3,13 +3,13 @@ package edu.buet.cse.spring.ch05.v1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import edu.buet.cse.spring.ch05.v1.dao.ChirperDaoImpl;
+import edu.buet.cse.spring.ch05.v1.dao.ChirperDao;
 import edu.buet.cse.spring.ch05.v1.model.Message;
 
 public class App3 {
   public static void main(String... args) {
     ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch05/v1/spring-beans.xml");
-    ChirperDaoImpl chirperDao = appContext.getBean("chirperDao", ChirperDaoImpl.class);
+    ChirperDao chirperDao = appContext.getBean("chirperDao", ChirperDao.class);
     Message message = chirperDao.getMessage(1L);
     System.out.println(message);
   }
