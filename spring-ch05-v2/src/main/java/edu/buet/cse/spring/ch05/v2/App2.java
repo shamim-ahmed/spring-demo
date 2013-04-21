55a6492fd5338a2ed11cc5919042a07cdc92326f
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-21 23:57:29 +0600

    now DAO implementation is retrieved from the application context through its interface name

--- spring-ch05-v2/src/main/java/edu/buet/cse/spring/ch05/v2/App2.java
+++ spring-ch05-v2/src/main/java/edu/buet/cse/spring/ch05/v2/App2.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import edu.buet.cse.spring.ch05.v2.dao.ChirperDaoImpl;
+import edu.buet.cse.spring.ch05.v2.dao.ChirperDao;
 import edu.buet.cse.spring.ch05.v2.model.Message;
 
 public class App2 {
   public static void main(String... args) {
     ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch05/v2/spring-beans.xml");
-    ChirperDaoImpl chirperDao = appContext.getBean("chirperDao", ChirperDaoImpl.class);
+    ChirperDao chirperDao = appContext.getBean("chirperDao", ChirperDao.class);
     List<Message> messages = chirperDao.getLatestMessages(5);
     
     for (Message m : messages) {
