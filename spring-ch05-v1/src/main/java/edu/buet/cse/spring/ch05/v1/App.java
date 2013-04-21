f3cc53ccdda420ee2457a8fa88ae287884af4287
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-21 15:21:28 +0600

    added the skeleton of a new project

---
+++ spring-ch05-v1/src/main/java/edu/buet/cse/spring/ch05/v1/App.java
@@ -0,0 +1,13 @@
+package edu.buet.cse.spring.ch05.v1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
