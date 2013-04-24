1d5c476f532988d21cf9fae5a390af5865f7557c
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-24 20:23:57 +0600

    updated one class and several mapping resources to indicate the uniqueness of username field of User

--- spring-ch05-v6/src/main/java/edu/buet/cse/spring/ch05/v6/model/User.java
+++ spring-ch05-v6/src/main/java/edu/buet/cse/spring/ch05/v6/model/User.java
@@ -25,7 +25,7 @@ public class User {
   @Column(name = "id")
   private Long id;
   
-  @Column(name = "username", nullable = false)
+  @Column(name = "username", nullable = false, unique = true)
   private String username;
   
   @Column(name = "password", nullable = false)
