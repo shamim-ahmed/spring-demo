ff40a37d052ea4fb42f1281fd9df82e9951c43f6
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-25 20:24:29 +0600

    implemented the DAO object functionality

--- spring-ch07-v1/src/main/java/edu/buet/cse/spring/ch07/v1/dao/ChirperDaoImpl.java
+++ spring-ch07-v1/src/main/java/edu/buet/cse/spring/ch07/v1/dao/ChirperDaoImpl.java
@@ -2,42 +2,57 @@ package edu.buet.cse.spring.ch07.v1.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.buet.cse.spring.ch07.v1.model.Message;
 import edu.buet.cse.spring.ch07.v1.model.User;
 
-@Component("chirperDao")
+@Repository("chirperDao")
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class ChirperDaoImpl implements ChirperDao {
-
-  @Override
-  public User getUser(Long id) {
-    // TODO Auto-generated method stub
-    return null;
+  private final HibernateTemplate hibernateTemplate;
+  
+  @Autowired
+  public ChirperDaoImpl(HibernateTemplate hibernateTemplate) {
+    this.hibernateTemplate = hibernateTemplate;
   }
 
   @Override
+  public User getUser(Long id) {
+    User user = (User) hibernateTemplate.get(User.class, id);
+    return user;
+  }
+
+  @Override
+  @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
   public boolean createUser(User user) {
-    // TODO Auto-generated method stub
-    return false;
+    Long id = (Long) hibernateTemplate.save(user);
+    return id != null;
   }
 
   @Override
   public Message getMessage(Long id) {
-    // TODO Auto-generated method stub
-    return null;
+    Message message = (Message) hibernateTemplate.get(Message.class, id);
+    return message;
   }
 
   @Override
   public List<Message> getLatestMessages(int count) {
-    // TODO Auto-generated method stub
-    return null;
+    hibernateTemplate.setMaxResults(count);
+    @SuppressWarnings("unchecked")
+    List<Message> messages = hibernateTemplate.find("from Message m order by m.id desc");
+    
+    return messages;
   }
 
   @Override
+  @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
   public boolean createMessage(Message message) {
-    // TODO Auto-generated method stub
-    return false;
+    Long id = (Long) hibernateTemplate.save(message);
+    return id != null;
   }
-  
 }
