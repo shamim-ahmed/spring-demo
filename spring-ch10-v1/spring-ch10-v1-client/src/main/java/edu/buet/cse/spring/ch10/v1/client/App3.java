772bd5a297cc98b69a5d76ee7fed37cd96b75eb5
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-05-04 23:15:15 +0600

    - moved the SQL script to the spring-ch10-v1-common module - added a new class that retrieves a list of users through the expoted service over RMI

---
+++ spring-ch10-v1/spring-ch10-v1-client/src/main/java/edu/buet/cse/spring/ch10/v1/client/App3.java
@@ -0,0 +1,21 @@
+package edu.buet.cse.spring.ch10.v1.client;
+
+import java.util.Collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.buet.cse.spring.ch10.v1.model.User;
+import edu.buet.cse.spring.ch10.v1.service.ChirperService;
+
+public class App3 {
+  public static void main(String... args) {
+    ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch10/v1/client/spring-beans.xml");
+    ChirperService chirperService = appContext.getBean("chirperService", ChirperService.class);
+    Collection<User> users = chirperService.getUsers(5);
+    
+    for (User user : users) {
+      System.out.println(user);
+    }
+  }
+}
