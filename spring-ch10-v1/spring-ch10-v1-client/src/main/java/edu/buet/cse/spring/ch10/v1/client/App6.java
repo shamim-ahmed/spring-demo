21b0897c6ac437c178294b3ac3118fd387a4e680
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-05-05 00:58:45 +0600

    added a new class that creates a new message through the exported service over RMI

---
+++ spring-ch10-v1/spring-ch10-v1-client/src/main/java/edu/buet/cse/spring/ch10/v1/client/App6.java
@@ -0,0 +1,30 @@
+package edu.buet.cse.spring.ch10.v1.client;
+
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.buet.cse.spring.ch10.v1.model.Message;
+import edu.buet.cse.spring.ch10.v1.service.ChirperService;
+
+public class App6 {
+  public static void main(String... args) {
+    ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch10/v1/client/spring-beans.xml");
+    ChirperService chirperService = appContext.getBean("chirperService", ChirperService.class);
+    Message message = new Message();
+    message.setContent("This is a test");
+    message.setCreationDate(new Date());
+    message.setUserId(1L);
+    
+    boolean result = false;
+    
+    try {
+      result = chirperService.addMessage(message);
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
+    }
+    
+    System.out.printf("The message %s created successfully%n", result ? "was" : "was not");
+  }
+}
