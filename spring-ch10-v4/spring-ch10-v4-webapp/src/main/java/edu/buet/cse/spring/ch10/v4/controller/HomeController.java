7066d608fd770eb3785c7abdc465fb0b88321d71
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-05-06 13:02:09 +0600

    now the web application that exports the ChirperService through SOAP can also be accessed from a browser

---
+++ spring-ch10-v4/spring-ch10-v4-webapp/src/main/java/edu/buet/cse/spring/ch10/v4/controller/HomeController.java
@@ -0,0 +1,18 @@
+package edu.buet.cse.spring.ch10.v4.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller("homeController")
+public class HomeController extends AbstractController {
+
+  @Override
+  protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mv = new ModelAndView("home");
+    return mv;
+  }
+}
---
+++ spring-ch10-v4/spring-ch10-v4-webapp/src/main/java/edu/buet/cse/spring/ch10/v4/controller/UserController.java
@@ -0,0 +1,33 @@
+package edu.buet.cse.spring.ch10.v4.controller;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import edu.buet.cse.spring.ch10.v4.model.User;
+import edu.buet.cse.spring.ch10.v4.service.ChirperService;
+
+@Controller("userController")
+public class UserController extends AbstractController {
+  private final ChirperService chirperService;
+  
+  @Autowired
+  public UserController(ChirperService chirperService) {
+    this.chirperService = chirperService;
+  }
+  
+  @Override
+  protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mv = new ModelAndView("userList");
+    Collection<User> users = chirperService.getUsers(Integer.MAX_VALUE);
+    mv.addObject("users", users);
+    
+    return mv;
+  }
+}
