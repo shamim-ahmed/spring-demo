ae1d0b2d592c400eab45a9f1946ef1b23851c77a
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-05-08 16:23:50 +0600

    Now tweets are produced by selecting a message from a pre-defined array of messages

--- spring-ch12-v5/spring-ch12-v5-actor/src/main/java/edu/buet/cse/spring/ch12/v5/actor/Producer.java
+++ spring-ch12-v5/spring-ch12-v5-actor/src/main/java/edu/buet/cse/spring/ch12/v5/actor/Producer.java
@@ -1,6 +1,7 @@
 package edu.buet.cse.spring.ch12.v5.actor;
 
 import java.util.Date;
+import java.util.Random;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,11 +10,20 @@ import edu.buet.cse.spring.ch12.v5.model.Tweet;
 import edu.buet.cse.spring.ch12.v5.service.LoggingService;
 
 public class Producer {
+  private static final String[] MESSAGES = {"In God we trust, others must pay cash",
+                                            "There is no place like www.home.com",
+                                            "You are unique, just like everyone else",
+                                            "Linux forever"};
+  
   public static void main(String[] args) {
     ApplicationContext appContext = new ClassPathXmlApplicationContext("/edu/buet/cse/spring/ch12/v5/spring-beans.xml");
     LoggingService loggingService = appContext.getBean("loggingService", LoggingService.class);
     
-    Tweet tweet = new Tweet("This is a test", new Date());
+    Random randomGenerator = new Random();
+    int index = randomGenerator.nextInt(MESSAGES.length);
+    
+    Tweet tweet = new Tweet(MESSAGES[index], new Date());
     loggingService.log(tweet);
+    System.out.println("Tweet logged successfully");
   }
 }
